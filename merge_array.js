abe1b0d365ba0322eb4d03e01ad25307061a16f7
omur8819 <67431733+omur8819@users.noreply.github.com>
2020-10-14 22:38:13 +0300

    project-update

---
+++ merge_array.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click", mergeArray);
+
+function mergeArray() {
+    const array1 = document.querySelector("#array1").value.trim(" ");
+    const array2 = document.querySelector("#array2").value.trim(" ");
+};
